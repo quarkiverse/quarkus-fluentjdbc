bc79ff0bf805381fa8ccdc4ace18d6453fb0e23a
serkan.eskici <serkan.eskici@notarisid.nl>
2025-03-20 14:55:51 +0100

    removed IT test

--- integration-tests/src/test/java/io/quarkiverse/fluentjdbc/it/FluentjdbcResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.fluentjdbc.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class FluentjdbcResourceIT extends FluentjdbcResourceTest {
-}
